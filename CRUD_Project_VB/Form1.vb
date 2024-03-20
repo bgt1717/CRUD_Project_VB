04e6e8f9e0ee2a1b894db56a4571240f8dcb0bc1
bgt1717 <bgt17@txtstate.edu>
2024-03-20 12:59:58 -0600

    Integers and Strings initialized.

--- CRUD_Project_VB/Form1.vb
+++ CRUD_Project_VB/Form1.vb
@@ -3,6 +3,17 @@
 
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim pid As Integer = Textproductid.Text
+        Dim iname As String = Textitemname.Text
+        Dim design As String = Textdesign.Text
+        Dim color As String = Combocolor.Text
+        Dim insertdate As DateTime = DateTimePickerinsert.Text
+        Dim wtype As String = ""
+        If Radioallowed.Checked = True Then
+            wtype = "Allowed"
+        Else
+            wtype = "Not Allowed"
+        End If
         Dim con As New SqlConnection("Data Source=(localdb)\Local;Initial Catalog=ProgrammingDB;Integrated Security=True;Encrypt=True;Trust Server Certificate=True")
         con.Open()
         Dim command As New SqlCommand("Insert into Product_Setup_Tab values()")
