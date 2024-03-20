5c09661ea44e0aa488c9c76d0bceaec96e8e7ecb
bgt1717 <bgt17@txtstate.edu>
2024-03-20 13:55:03 -0600

    Submission/Create completion

--- CRUD_Project_VB/Form1.vb
+++ CRUD_Project_VB/Form1.vb
@@ -1,31 +1,35 @@
 ﻿Imports System.Data.SqlClient
 
-
 Public Class Form1
-    Dim con As New SqlConnection("Data Source=(localdb)\Local;Initial Catalog=ProgrammingDB;Integrated Security=True;Encrypt=True;Trust Server Certificate=True")
+    Dim con As New SqlConnection("Data Source=(localdb)\Local;Initial Catalog=ProgrammingDB;Integrated Security=True;")
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim pid As Integer = Textproductid.Text
         Dim iname As String = Textitemname.Text
         Dim design As String = Textdesign.Text
         Dim color As String = Combocolor.Text
-        Dim insertdate As DateTime = DateTimePickerinsert.Text
-        Dim wtype As String = ""
-        If Radioallowed.Checked = True Then
-            wtype = "Allowed"
-        Else
-            wtype = "Not Allowed"
-        End If
+        Dim insertdate As DateTime = DateTimePickerinsert.Value
+        Dim wtype As String = If(Radioallowed.Checked, "Allowed", "Not Allowed")
 
         con.Open()
-        Dim command As New SqlCommand("Insert into Product_Setup_Tab values('" & pid & "','" & iname & "','" & design & "','" & color & "','" & insertdate & "','" & wtype & "',)", con)
+        Dim command As New SqlCommand("INSERT INTO Product_Setup_Tab VALUES(@pid, @iname, @design, @color, @insertdate, @wtype)", con)
+        command.Parameters.AddWithValue("@pid", pid)
+        command.Parameters.AddWithValue("@iname", iname)
+        command.Parameters.AddWithValue("@design", design)
+        command.Parameters.AddWithValue("@color", color)
+        command.Parameters.AddWithValue("@insertdate", insertdate)
+        command.Parameters.AddWithValue("@wtype", wtype)
         command.ExecuteNonQuery()
+        con.Close()
         MessageBox.Show("Submission Successful")
         LoadDataInGrid()
     End Sub
+
     Private Sub LoadDataInGrid()
-        Dim command As New SqlCommand("select * from Product_Setup_Tab", con)
+        Dim command As New SqlCommand("SELECT * FROM Product_Setup_Tab", con)
         Dim sda As New SqlDataAdapter(command)
         Dim dt As New DataTable
+        sda.Fill(dt)
         DataGridView1.DataSource = dt
     End Sub
 
@@ -33,3 +37,4 @@ Public Class Form1
         LoadDataInGrid()
     End Sub
 End Class
+
